ccea9bac893e6708fe24e0753aa530b94a8b929e
Tari123 <asrilestari121@gmail.com>
2020-06-18 19:07:44 +0700

    menambahkan list

--- Index.php
+++ Index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Git Remote</title>
 </head>
+
 <body>
     <hl> hello world; </hl>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim distinctio repudiandae qui amet, ipsam nobis hic ratione libero error consectetur ea minima rem illo consequuntur iusto vel. Delectus, aliquam dolore?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
+
 </html>
